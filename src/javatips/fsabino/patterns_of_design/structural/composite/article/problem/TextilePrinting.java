5dabaa47880632e752f0d808b56fc4ce9a647f1b
Franco <fjsabino022@gmail.com>
2018-04-12 13:39:04 -0300

    Composite Pattern Exercise

---
+++ src/javatips/fsabino/patterns_of_design/structural/composite/article/problem/TextilePrinting.java
@@ -0,0 +1,19 @@
+package javatips.fsabino.patterns_of_design.structural.composite.article.problem;
+
+public class TextilePrinting extends Product {
+	
+	private String type;
+
+    public TextilePrinting(int id, String reference, String type) {
+        super(id, reference);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/javatips/fsabino/patterns_of_design/structural/composite/article/solution/ArticleComponent.java
@@ -0,0 +1,23 @@
+package javatips.fsabino.patterns_of_design.structural.composite.article.solution;
+
+public abstract class ArticleComponent {
+	private String icon;
+
+    public ArticleComponent(String icon) {
+        this.icon = icon;
+    }
+    
+    public abstract boolean isComposite();
+    
+    public abstract void add(ArticleComponent articleComponent);
+    
+    public abstract void remove(ArticleComponent articleComponent);
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
